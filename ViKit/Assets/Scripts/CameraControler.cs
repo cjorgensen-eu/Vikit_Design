a5eee6120db70a39be1ab073bb00c4df2c25dd9f
Jorgensen <register@cjorgensen.eu>
2021-01-09 00:30:44 +0100

    Added simple camera control (zoom + rrotations)

---
+++ ViKit/Assets/Scripts/CameraControler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraControler : MonoBehaviour
+{
+
+    private void OnScrollWheel(InputValue inputValue)
+    {
+        Vector2 scrollVector = inputValue.Get<Vector2>();
+
+        transform.Translate(0, 0, scrollVector.y * 0.01f, Space.Self);
+    }
+
+    void Update()
+    {
+        if (Mouse.current.rightButton.isPressed)
+        {
+            Vector2 delta = Mouse.current.delta.ReadValue();
+            transform.RotateAround(Vector3.zero,Vector3.left, delta.y);
+            transform.RotateAround(Vector3.zero, Vector3.up, delta.x);
+        }
+
+
+
+
+    }
+}
